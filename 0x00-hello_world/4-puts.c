b1fc59e98b5db5032b1dd45f708b163ac15cb1d9
MIKEY1221 <MIKEY1221>
2023-09-08 12:42:44 -0700

    all

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+iinclude<studio.h>
+/**
+ * main entry point
+ *
+ * returns 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return 0; 
+}
+
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/*
+ * main entry
+ * *return 0 on succss
+ */
+int main(void)
+{
+	prinf("Size of a char: %d byte(s)\n",sizeof(char));
+	prinf("Size of a int: %d byte(s)\n",sizeof(int));
+	prinf("Size of a long int: %d byte(s)\n",sizeof(long int));
+	prinf("Size of a long long int: %d byte(s)\n",sizeof(long long int));
+	prinf("Size of a float: %d byte(s)\n",sizeof(float));	
